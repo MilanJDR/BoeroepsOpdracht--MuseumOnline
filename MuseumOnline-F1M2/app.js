f5d638b4684447bcf192d1ead82ce0b902a32567
Jamie <31694@ma-web.nl>
2021-01-15 15:13:40 +0100

    Edits

--- MuseumOnline-F1M2/app.js
+++ MuseumOnline-F1M2/app.js
@@ -4,9 +4,9 @@ controls.enableMovement();
 
 //create a model
 const melkMeisje = new Model('assets/models/vermeer/export_museumprops.babylon.gltf');
-melkMeisje.setPosition(-6.02, 0.920, -3.21);
+melkMeisje.setPosition(-6.138, 0.888, -5.329);
 melkMeisje.setScale(100, 100, 100);
-melkMeisje.setRotation(0, -36.9, 0);
+melkMeisje.setRotation(0, -300.000, 0); 
 
 //create cuberoom
 const cuberoom = new Model('assets/models/cube-room/cube-room.gltf')
@@ -16,17 +16,11 @@ cuberoom.setRotation(0, -36.95, 0);
 
 //create some text
 const lamText = new Text('Lisser Art Museum');
-lamText.setPosition(-7.971, 3.746, -4.49);
+lamText.setPosition(-8.047, 3.746, -5.447);
 lamText.setFontsize(11);
 lamText.setScale(0.5, 0.5, 0.5)
 lamText.setRotation(0, 50.000, 0);
 lamText.setColor('white')
-
-//create an Logo
-const LamLogo = new XRImage('asssets/images/LogoLam.jpg');
-LamLogo.setPosition(-.4,2.8,-4.5)
-LamLogo.setScale(3,1,1)
-LamLogo.setRotation(0,0,0) 
 
 //create an image1
 const image1 = new XRImage('assets/images/Lisse-049.jpg');
@@ -34,29 +28,36 @@ image1.setPosition(2.413,  2.288, -4.997);
 image1.setRotation(-0.740, -34.670, 0);
 image1.setScale(1.5, 1.5, 1.5);
 
-//create an image2 
-const image2 = new XRImage('assets/images/FolkertdeJong.jpg');
-image2.setPosition(-2.5, 0.910, -3.215);
-image2.setRotation(0, -36.9, -1);
+//create an image2
+const image2 = new XRImage('assets/images/Lisse-051.jpg');
+image2.setPosition(-2.270, 2.288, 2.531);
+image2.setRotation(0, -40, 0);
 image2.setScale(1.5, 1.5, 1.5);
 
 //create an image3
-const image3 = new XRImage('assets/images/Lisse-043.jpg');
-image3.setPosition(1.751, 2.230, 0.779);
-image3.setRotation(0, -127, 0);
+const image3 = new XRImage('assets/images/Lisse-050.jpg');
+image3.setPosition(1.709, 2.288, 0.837);
+image3.setRotation(0, 52.430, 0);
 image3.setScale(1.5, 1.5, 1.5);
 
 //create an image4
-const image4 = new XRImage ('assets/images/BadGrapes.jpg')
-image4.setPosition(-2.373, 2.180, 2.415);
-image4.setRotation(0,-35.150, 0);
-image4.setScale(1.5,1.5,1.5);
+const image4 = new XRImage('assets/images/Lisse-052.jpg');
+image4.setPosition(-5.052, 2.288, 0.454);
+image4.setRotation(-0.740, -34.670, 0);
+image4.setScale(1.5, 1.5, 1.5);
+
+//create an image5
+const image5 = new XRImage('assets/images/Lisse-053.jpg');
+image5.setPosition(-2.844, 2.288, -8.925);
+image5.setRotation(-0.740, -40.00, 0);
+image5.setScale(1.5, 1.5, 1.5);
+
 
 //create a primitive (cylinder)
 const cylinder = new Cylinder();
 cylinder.setColor('grey')
 cylinder.setRadius(0.5);
-cylinder.setPosition(-2.489, 3.988, 3.207);
+cylinder.setPosition(0, 1.414, 1.365);
 cylinder.setScale(1.5, 8.640, 1.5,)
 
 //create a 360 image
